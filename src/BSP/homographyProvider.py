63d3d51d7d4302249cdde9bea68bbadedc2e23ff
olfarmer <mail@clemens.jann.xyz>
2021-11-23 12:38:18 +0100

    Feat: BoardOrientation and homographyProvider

--- src/BSP/homographyProvider.py
+++ src/BSP/homographyProvider.py
@@ -3,6 +3,8 @@ import numpy as np
 import typing
 import matplotlib.pyplot as plt
 
+from src.BSP.BoardOrientation import BoardOrientation
+
 
 def calc_scale(crn_pts_src, crn_pts_dst):
     """
@@ -29,22 +31,74 @@ def scale_point(point, scaling):
     return scaled_point
 
 
-def get_led_roi(homography_matrix, crn_pts, reference_hw, led_center):
+def homography_by_sift(ref_img, target_img, distance_factor=0.85, display_result=False) -> BoardOrientation:
     """
-        Returns the LEDs in the target image based on the homography matrix
-    :param homography_matrix: The homography matrix which translates points from the reference to the target
-    :param crn_pts: The calculated corner points of the target board
+    Calculates the board orientation based on SIFT with knnMatch
+    :param ref_img: The reference image for the calculation
+    :param target_img: The target image for the calculation
+    :param distance_factor: Influences the max distance of the matches as per Loew's ration test. A higher value means
+     more distant matches are also included. The optimal value may differ based on the board and image
+    :param display_result: If true the result is plotted
+    :return: A BoardOrientation object which contains the homography matrix and the corners
+    """
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(ref_img, None)
+    kp2, des2 = sift.detectAndCompute(target_img, None)
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m, n in matches:
+        if m.distance < distance_factor * n.distance:
+            good.append(m)
+
+    homography_matrix = None
+    dst = None
+    if len(good) > 10:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        homography_matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+        matches_mask = mask.ravel().tolist()
+        h, w, d = ref_img.shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])
+        dst = cv2.perspectiveTransform(np.array([pts]), homography_matrix)[0]
+        dst[1], dst[3] = dst[3], dst[1]
+    else:
+        print("Not enough matches are found - {}/{}".format(len(good), 10))
+        matches_mask = None
+
+    if display_result:
+        draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                       singlePointColor=None,
+                       matchesMask=matches_mask,  # draw only inliers
+                       flags=2)
+        img3 = cv2.drawMatches(ref_img, kp1, target_img, kp2, good, None, **draw_params)
+        plt.imshow(img3, 'gray'), plt.show()
+
+    return BoardOrientation(homography_matrix, dst)
+
+
+def get_led_roi(board_orientation, reference_hw, led_center):
+    """
+     Returns the LEDs in the target image based on the homography matrix
+    :param board_orientation: The orientation of the board in a BoardOrientation object
     :param reference_hw: A tuple with the height and width of the reference board
     :param led_center: A numpy list with the x,y coordinates of the centers of the LEDs
     :return: The LEDs in the target image
     """
 
     # Calculates the scaling between the reference and the target board
-    scale_x = abs(crn_pts[0][0] - crn_pts[2][0]) / reference_hw[0]
-    scale_y = abs(crn_pts[0][1] - crn_pts[1][1]) / reference_hw[1]
+    scale_x = abs(board_orientation.corners[0][0] - board_orientation.corners[2][0]) / reference_hw[0]
+    scale_y = abs(board_orientation.corners[0][1] - board_orientation.corners[1][1]) / reference_hw[1]
 
     # Transforms the center points
-    led_center = cv2.perspectiveTransform(np.array([led_center]), homography_matrix)[0]
+    led_center = cv2.perspectiveTransform(np.array([led_center]), board_orientation.homography_matrix)[0]
     radius = round(5 * max(scale_x, scale_y))
     leds = led_by_circle_coordinates(led_center.astype(int), radius)
 
