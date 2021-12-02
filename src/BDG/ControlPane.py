4bfdce4778b34ee70d6af4e0e3e82cf34b8ea20f
olfarmer <mail@clemens.jann.xyz>
2021-12-02 15:31:48 +0100

    feat: toolbar

--- src/BDG/ControlPane.py
+++ src/BDG/ControlPane.py
@@ -20,7 +20,34 @@ class ControlPane(tk.Frame):
         editMenu = tk.Menu(menu)
         menu.add_cascade(label="Edit", menu=editMenu)
 
-        self.imagePane = ImagePane(container)
+        toolbar = tk.Frame(self.master, relief=tk.RAISED)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        names = tk.Frame(self.master, relief=tk.RAISED)
+        names.pack(side=tk.RIGHT, fill=tk.X)
+        bottom = tk.Label(names, text="Fancy toolbar")
+        bottom.pack(side=tk.RIGHT)
+
+
+        #self.window = tk.PanedWindow(orient=tk.HORIZONTAL)
+        #self.window.pack(fill="both", expand="True")
+        #self.window.add(self)
+
+
+        #self.window.add(bottom)
+
+        v = tk.IntVar()
+        r1 = tk.Radiobutton(toolbar, text="Place corner point", value=1, variable=v)
+        r2 = tk.Radiobutton(toolbar, text="Place LED", value=2, variable=v)
+        r1.pack(side=tk.LEFT, fill=None, expand=False)
+        r2.pack(side=tk.LEFT, fill=None, expand=False)
+        #self.window.add(r1)
+        #self.window.add(r2)
+
+        self.imagePane = ImagePane(self.master)
+        self.imagePane.pack(side=tk.LEFT)
+
+
 
         editMenu.add_command(label="Undo", command=self.imagePane.undo_point)
         editMenu.add_command(label="Redo", command=self.imagePane.redo_point)
@@ -29,6 +56,8 @@ class ControlPane(tk.Frame):
         fileMenu.add_command(label="save", command=self.save_image)
 
         menu.add_command(label="Toggle mode", command=self.imagePane.toggle_state)
+        menu.add_command(label="Test", command=lambda: self.imagePane.choose_image(
+            "/home/cj7/Desktop/LED-Detection/src/prototyping/resources/ref.jpg"))
 
     def chooseImage(self):
         path = askopenfilename()
