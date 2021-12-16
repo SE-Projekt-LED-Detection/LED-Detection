68c9887e9db8d0cdfd783f5748663165c127b788
Christoph Meyer <christoph.meyer@em-perform.de>
2021-12-16 12:22:22 +0100

    feature: add file_handler

--- src/BDG/coordinator/coodinatior.py
+++ src/BDG/coordinator/coodinatior.py
@@ -1,7 +1,17 @@
 from model.board_model import Board
-
+from file_handler import FileHandler
+from edit_handler import EditHandler
 class Coordinator():
-    def __init__():
+    def __init__(self):
         self.board = Board();
 
-        self.edit = 
+        self.file_handler = FileHandler(self)
+        self.edit_handler = EditHandler(self.board)
+
+    
+    def update_board(self,board: Board):
+        self.board = board
+    
+
+
+    
--- src/BDG/coordinator/edit_handler.py
+++ src/BDG/coordinator/edit_handler.py
@@ -1,3 +1,9 @@
 
+from model.board_model import Board
+import utils.json_util as js_util
+class EditHandler():
+    def __init__(self,board: Board) -> None:
+        self.board = Board
 
-class
+    
+
