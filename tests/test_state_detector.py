308d8b0d78b4757ea05305a2c6a1f4248061728e
olfarmer <mail@clemens.jann.xyz>
2022-02-15 11:24:36 +0100

    test: add fail condition on exception

--- tests/test_state_detector.py
+++ tests/test_state_detector.py
@@ -1,6 +1,8 @@
 import threading
 import time
 
+import pytest
+
 from src.BDG.model.board_model import Board
 from src.BSP.BufferlessVideoCapture import BufferlessVideoCapture
 from src.BSP.state_detector import StateDetector
@@ -14,17 +16,21 @@ def test_blackbox_state_detector_with_zcu102():
     Runs the StateDetector with an example video.
     """
 
-    reference = jsutil.from_json(file_path="./resources/ZCU102/reference/ref.json")
+    try:
+        reference = jsutil.from_json(file_path="./resources/ZCU102/reference/ref.json")
 
-    dec = StateDetector(reference, 4)
+        dec = StateDetector(reference, 4)
 
-    cap = MockVideoCapture("./resources/ZCU102/zcu102_video.avi", False)
-    dec.open_stream(cap)
+        cap = MockVideoCapture("./resources/ZCU102/zcu102_video.avi", False)
+        dec.open_stream(cap)
 
-    th = threading.Thread(target=dec.start)
-    th.start()
+        th = threading.Thread(target=dec.start)
+        th.start()
 
-    th.join()
+        th.join()
+
+    except Exception as e:
+        pytest.fail(e)
 
 def test_blackbox_state_detector():
     reference = jsutil.from_json(file_path="resources/pi_test.json")
