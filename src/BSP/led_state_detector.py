a248e9337d08cfc96b4b46d2e6e2b3b3be6f6822
olfarmer <mail@clemens.jann.xyz>
2022-02-15 10:59:44 +0100

    feat: improve dummy on/off detection

--- src/BSP/led_state_detector.py
+++ src/BSP/led_state_detector.py
@@ -1,10 +1,18 @@
 import time
 
+import numpy as np
+from cv2 import cv2
+
 from src.BSP.led_state import LedState
 
 
-def get_state(led_roi, colors):
-    state = LedState("on", "red", time.time_ns())
+def get_state(led_roi: np.array, colors):
+
+    #led = cv2.cvtColor(led_roi, cv2.COLOR_RGB2HSV)
+
+    mean = np.nanmean(led_roi[..., 2])
+
+    state = LedState("on" if mean > 190 else "off", "red", time.time())
 
     # TODO: return real values instead of dummy values
 
