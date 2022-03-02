cdb573280d39b4638b2b69cd4bae521c114f6caf
Christoph Meyer <christoph.meyer@em-perform.de>
2022-03-02 11:59:05 +0100

    feat: add mqtt connector

--- src/BIP/connection/config.py
+++ src/BIP/connection/config.py
@@ -9,4 +9,5 @@ class Config:
     def __init__(self, broker_address: str, broker_port: int, topics=""):
         self.broker_address = broker_address
         self.broker_port = broker_port
+        self.broker_keepalive = 60
         self.topics = topics
--- src/BIP/connection/mqtt_connector.py
+++ src/BIP/connection/mqtt_connector.py
@@ -1,11 +1,13 @@
 import logging
-import time
-from datetime import datetime
+
 
 import paho.mqtt.client as mqtt
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s', )
+
+host = "89.58.3.45"
+port = 1883
 
 """
 This class is used to publish messages to a MQTT broker.
@@ -34,28 +36,40 @@ This class is used to publish messages to a MQTT broker.
 """
 
 
-class Board_Changes():
+class BoardChanges():
     def __init__(self, changes_dict):
         self.id = changes_dict["id"]
         self.time = changes_dict["time"]
         self.changes = changes_dict["changes"]
 
 
-class Mqtt_Publisher():
+class MqttConnector():
     """
     Class for publishing messages to MQTT broker.
     """
 
     def __init__(self, config):
+        if config is None:
+            raise Exception("No config provided")
+        self.config = config
         self.client = mqtt.Client()
-        self.client.on_connect = lambda client, userdata, flags, rc: logging.info(
-            f"Connect to Broker\n{client},{userdata},{flags},{rc}")
-        self.client.on_publish = lambda client, userdata, mid: logging.info(
-            f"Publish message\n{mid}")
+        # register callbacks
+        self.on_connect()
+        self.on_publish()
+        # connect to broker
+        self.connect()
+        # start loop
+        self.loop_start()
 
-        self.client.connect(config["broker_address"], config["broker_port"])
+    def connect(self):
+        self.client.connect(self.config["broker_address"], self.config["broker_port"])
 
     def publish_changes(self, changes):
+        """
+        publish changes to MQTT broker
+        :param changes: list of BoardChanges
+        :return:
+        """
         for board in changes["boards"]:
             topic = board["id"]
             payload = board["time"]
@@ -63,15 +77,17 @@ class Mqtt_Publisher():
             for change in board["changes"]:
                 change_topic = topic + "/" + change["id"] + "/" + change["value"] + "/" + change["color"]
                 self.client.publish(change_topic, payload)
+                print(f"Publish message\n{change_topic},{payload}")
+
+    def on_connect(self):
+        self.client.on_connect = lambda client, userdata, flags, rc: logging.info(
+            f"Connect to Broker\n{client},{userdata},{flags},{rc}")
+
+    def on_publish(self):
+        self.client.on_publish = lambda client, userdata, mid: logging.info(
+            f"Publish message\n{mid}")
+
+    def subscribe_to_config(self):
+        self.client.subscribe(self.config["config"])
 
 
-if __name__ == '__main__':
-    mqtt_publisher = Mqtt_Publisher({"broker_address": "localhost", "broker_port": 1883})
-
-    for i in range(10):
-        time.sleep(2)
-        led_name = str(i%2)
-        led_name = "LED-" + led_name
-        mqtt_publisher.publish_changes({"boards": [{"id": "raspberrypi", "time": str(datetime.now()), "changes": [{"id": led_name, "value": "on", "color": "red"}]}]})
-
-    mqtt_publisher.client.disconnect()
