4fc8feb6a55c57b9b916026f40567be9e3574170
olfarmer <mail@clemens.jann.xyz>
2022-03-29 11:58:23 +0200

    ref: move UrlBoardLoader to new util module

--- tests/test_uri_board_loader.py
+++ tests/test_uri_board_loader.py
@@ -1,6 +1,6 @@
 import os
 
-import BSP.UrlBoardLoader
+import BSP.util.UrlBoardLoader
 
 
 def test_load_board():
@@ -9,7 +9,7 @@ def test_load_board():
     """
     test_url = "https://github.com/SE-Projekt-LED-Detection/LED-Detection/raw/7b105b37c7d97f256fde8be933bfa5a55988f2b9/tests/resources/ZCU102/reference/ref.zip"
     path = os.path.dirname(os.path.abspath(__file__))
-    board = BSP.UrlBoardLoader.load_board_from_url(path, test_url)
+    board = BSP.util.UrlBoardLoader.load_board_from_url(path, test_url)
 
     resources_path = os.path.join(path, "boards")
     config_file = os.path.join(resources_path, "ref", "ref.json")
