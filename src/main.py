124b2edc4ff7b346c8a18707b9ac822fb28ed7f5
olfarmer <mail@clemens.jann.xyz>
2022-04-05 12:20:20 +0200

    fix: naming of parameter

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+import logging
+
 import configargparse
 import os
 import sys
 
 def main():
     parser = configargparse.ArgParser(default_config_files=['./config.conf'], description='A led state provider state detecting different controller boards')
-    parser.add('-c', '--config', type=str,is_config_file=True, help='Path to config file. NOTE: this is not the reference path, but the path to the config file')
-    parser.add('-r', '--reference',required=True, type=str, help='Path to reference file')
+    parser.add('-c', '--config', type=str, is_config_file=True, help='Path to config file. NOTE: this is not the reference path, but the path to the config file')
+    parser.add('-r', '--reference', required=True, type=str, help='Path to reference file')
+    parser.add('-w', '--webcam_id', required=True, type=int, help='ID of the usb webcam')
     parser.add('-bh', '--broker_host', type=str, default='localhost', help='Broker host for MQTT')
     parser.add('-bp', '--broker_port', type=int, default=1883, help='Broker port for MQTT')
 
@@ -15,8 +18,6 @@ def main():
     parser.add('-ll', '--log_level', type=str, help='Enable logging', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO')
     parser.add('-lf', '--log_file', type=str, help='Enable logging to file', default='./led_state_provider.log')
     parser.add('-s', '--validity_seconds', type=int, default=300, help='The seconds until the homography matrix is calculated anew')
-
-
 
     args = parser.parse_args()
     print(args)
