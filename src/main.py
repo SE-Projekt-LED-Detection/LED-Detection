067f734c45ea41f6d2cf1161c89ea003bf18d124
olfarmer <mail@clemens.jann.xyz>
2022-04-06 14:55:04 +0200

    ref: ref parse_arguments

--- src/main.py
+++ src/main.py
@@ -9,22 +9,8 @@ import sys
 from BSP.state_detector import StateDetector
 import BDG.utils.json_util as jsutil
 
-def main():
-    parser = configargparse.ArgParser(default_config_files=['./config.conf'], description='A led state provider state detecting different controller boards')
-    parser.add('-c', '--config', type=str, is_config_file=True, help='Path to config file. NOTE: this is not the reference path, but the path to the config file')
-    parser.add('-r', '--reference', required=True, type=str, help='Path to reference file')
-    parser.add('-w', '--webcam_id', required=True, type=int, help='ID of the usb webcam')
-    parser.add('-bh', '--broker_host', type=str, default='localhost', help='Broker host for MQTT')
-    parser.add('-bp', '--broker_port', type=int, default=1883, help='Broker port for MQTT')
 
-    parser.add('-d', '--debug', action='store_true', help='Enable debug mode')
-    parser.add('-v', '--visualizer', action='store_true', help='activate visualizer mode')
-    parser.add('-l', '--log_to_console', action='store_true', help='Enable logging to console. Only necessary if log_file is set otherwise logging will be printed in the console by default')
-    parser.add('-ll', '--log_level', type=str, help='Enable logging', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO')
-    parser.add('-lf', '--log_file', type=str, help='Enable logging to file', default=None)
-    parser.add('-s', '--validity_seconds', type=int, default=300, help='The seconds until the homography matrix is calculated anew')
-
-    args = parser.parse_args()
+def main(args):
 
     logging.basicConfig(filename=args.log_file, filemode='w', level=args.log_level, format='%(levelname)s:%(message)s', force=True)
 
@@ -58,5 +44,28 @@ def main():
             return
 
 
+def parse_arguments():
+    parser = configargparse.ArgParser(default_config_files=['./config.conf'],
+                                      description='A led state provider state detecting different controller boards')
+    parser.add('-c', '--config', type=str, is_config_file=True,
+               help='Path to config file. NOTE: this is not the reference path, but the path to the config file')
+    parser.add('-r', '--reference', required=True, type=str, help='Path to reference file')
+    parser.add('-w', '--webcam_id', required=True, type=int, help='ID of the usb webcam')
+    parser.add('-bh', '--broker_host', type=str, default='localhost', help='Broker host for MQTT')
+    parser.add('-bp', '--broker_port', type=int, default=1883, help='Broker port for MQTT')
+
+    parser.add('-d', '--debug', action='store_true', help='Enable debug mode')
+    parser.add('-v', '--visualizer', action='store_true', help='activate visualizer mode')
+    parser.add('-l', '--log_to_console', action='store_true',
+               help='Enable logging to console. Only necessary if log_file is set otherwise logging will be printed in the console by default')
+    parser.add('-ll', '--log_level', type=str, help='Enable logging',
+               choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO')
+    parser.add('-lf', '--log_file', type=str, help='Enable logging to file', default=None)
+    parser.add('-s', '--validity_seconds', type=int, default=300,
+               help='The seconds until the homography matrix is calculated anew')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    main()
+    main(parse_arguments())
