700027f96c7a7d5036360243b151dec929db2366
Christoph Meyer <christoph.meyer@em-perform.de>
2022-04-06 13:15:38 +0200

    feat: add flask server

--- src/BSP/VideoStream/rtmp_output_stream.py
+++ src/BSP/VideoStream/rtmp_output_stream.py
@@ -3,37 +3,53 @@ import sys
 
 import ffmpeg
 import cv2
-import subprocess
+import logging as log
+
+
 
 video_format = "flv"
-server_url = "http://localhost:8080"
+server_url = "rtmp://localhost:8080"
 
 
 
 class VideoStream:
-    def __init__(self, url):
+    """
+    VideoStream class
+    for creating an rtmp stream out of opencv frames
+    """
+    def __init__(self, url, logger: log.Logger):
+
         self.url = url
         self.process = None
         self.queue = queue.Queue()
+        self.logger = logger
 
     def write(self, frame):
         """
         Writes the frame to the ffmpeg process
-        :param frame:
+        :param frame: is an opencv frame type: numpy.ndarray
         :return:
         """
         if not self.process:
             self.start_streaming(frame.shape[1], frame.shape[0])
-        self.process.stdin.write(frame.tobytes())
+        try:
+            self.process.stdin.write(frame.tobytes())
+        except IOError as e:
+            self.logger.error("Error writing to ffmpeg process: {}".format(e))
+            self.stop_streaming()
+        except BrokenPipeError as e:
+            self.logger.error("Error writing to ffmpeg process: {}".format(e))
+            self.stop_streaming()
+
 
 
 
 
     def start_streaming(self, width, height,fps=30):
         """
-        Starts the ffmpeg process to stream the video on th
-        :param width:
-        :param height:
+        Starts the ffmpeg process to stream the video and stores it in self.process
+        :param width: is the input and output width of the video type: int
+        :param height: is the input and output height of the video type: int
         :param fps:
         :return:
         """
@@ -41,7 +57,7 @@ class VideoStream:
             ffmpeg
             .input('pipe:', format='rawvideo',codec="rawvideo", pix_fmt='bgr24', s='{}x{}'.format(width, height))
             .output(
-                self.url + '/stream',
+                self.url,
                 #codec = "copy", # use same codecs of the original video
                 listen=1, # enables HTTP server
                 pix_fmt="yuv420p",
@@ -53,6 +69,9 @@ class VideoStream:
             .overwrite_output()
             .run_async(pipe_stdin=True)
         )
+        self.logger.log("Start streaming to {}".format(self.url))
+
+
 
 
     def stop_streaming(self):
@@ -63,6 +82,7 @@ class VideoStream:
         if self.process:
             self.process.kill()
             self.process = None
+            self.logger.log("Stopped streaming to {}".format(self.url))
 
 
 
@@ -84,6 +104,7 @@ def main():
             if ret:
                 video_stream.write(frame)
             else:
+                video_stream.stop_streaming()
                 break
     except KeyboardInterrupt:
         video_stream.stop_streaming()
