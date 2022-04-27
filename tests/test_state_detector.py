49cdb0e16bc0a2d4a27f7211158d7fb6f878caf9
Clemens Jann <mail@clemens.jann.xyz>
2022-04-27 15:12:57 +0200

    cleanup: remove zcu test

--- tests/test_state_detector.py
+++ tests/test_state_detector.py
@@ -11,35 +11,6 @@ from MockVideoCapture import MockVideoCapture
 from BSP.state_handler.state_table import get_state_table, get_last_entry, get_current_state, get_led_ids
 
 
-def test_blackbox_state_detector_with_zcu102():
-    """
-    Runs the StateDetector with an example video.
-    TODO: This test is not working, because the homography matrix is not correct.
-    """
-
-    reference = jsutil.from_json(file_path="./resources/ZCU102/reference/ref.json")
-
-    dec = StateDetector(reference=reference, webcam_id=4)
-
-    cap = MockVideoCapture("./resources/ZCU102/zcu102_video.avi", False)
-    dec.open_stream(cap)
-
-    dec._detect_current_state()
-
-    current_state = get_current_state()
-    led_ids = get_led_ids()
-    # Assert LEDs on and off based on the video
-    print(current_state)
-
-    assert current_state[current_state["led_id"] == led_ids[0]]["state"] == "off", "LED 0 not detected correctly"
-    assert current_state[current_state["led_id"] == led_ids[1]]["state"] == "on", "LED 1 not detected correctly"
-    assert current_state[current_state["led_id"] == led_ids[4]]["state"] == "on", "LED 4 not detected correctly"
-    assert current_state[current_state["led_id"] == led_ids[5]]["state"] == "on", "LED 5 not detected correctly"
-    assert current_state[current_state["led_id"] == led_ids[6]]["state"] == "off", "LED 6 not detected correctly"
-
-    cv2.waitKey(100)
-
-
 def test_blackbox_state_detector():
     reference = jsutil.from_json(file_path="resources/Pi/pi_test.json")
     with StateDetector(reference=reference, webcam_id=0, debug=True) as dec:
